ebceec9f2f12db8a19c7cc69007958795575f56d
tayyaba-ali <ta6402792@gmail.com>
2024-04-24 09:16:28 +0500

    variables

---
+++ class 1/app.js
@@ -0,0 +1,22 @@
+
+// alert("Welcome to JS world  ... Happy Coding!")
+// alert("Welcome to JS world  ... Happy Coding!")
+// alert("Welcome to JS world  ... Happy Coding!")
+// alert("Welcome to JS world  ... Happy Coding!")
+// alert("Welcome to JS world  ... Happy Coding!")
+
+//class
+
+// alert("hslfdhdsfj")  //using alert
+
+
+// console.log("79798797");
+
+var myName = "Tayyaba"; //camelcase
+
+console.log(myname)
+
+// Rule 1
+// Numbers are not allowed in starting
+//  space is not allowed
+// dollar and undescore special characters are allowed
