5c74d2d828acbc638c7e98f2bff35ab478538269
tayyaba-ali <ta6402792@gmail.com>
2024-05-01 10:59:27 +0500

    class 3 done

---
+++ class2/app.js
@@ -0,0 +1,32 @@
+// var saylaniStudentNumber;
+// var studentName = "Tayyaba Ali";
+
+// var myInstitue = "Saylani"
+// var instituteLocation = "Aliabad"
+
+// // Saylani is in Aliabad
+
+// console.log(myInstitue + " is in " + instituteLocation); //concatenation
+
+
+// string concatenation
+
+// var num1 = "34";
+// var alert = "735075408";
+// var Var = "mahi" //var
+
+
+// console.log(Nickname);
+
+
+// Question # 01
+
+// var username ;
+
+// Question # 02;
+
+var num =14;
+
+alert("You have visited " + num + " times")
+
+document.write("<i>Tayyaba <br> ototoutoutoutou</i>")
---
+++ class3/app.js
@@ -0,0 +1,29 @@
+
+// var num = 2+2;
+// console.log(152%3);
+
+
+// var d = 8;
+// var e = 3;
+// var f= 5;
+
+// var result = ++f  - e++ + d++ / f-- * ++d;
+
+
+
+// 20  16.3   17.2  24.9  24.5 
+  
+// console.log(((2+4) + (5-2))+8/3+4);  //Bodmas
+
+
+// var firstName = "23";
+// var num = 12;
+
+// console.log( typeof ( "2 plus 2 equals " + 2 + 2));
+
+//"2 plus 2 equals2" +2
+//
+
+var userData = +prompt("put your number")
+console.log(userData +2);
+
