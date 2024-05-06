c36e8c22c8ede0b973b06ea89f23ab2088d539d8
= <=>
2024-05-06 11:05:41 +0500

    class 4 done

---
+++ class4/app.js
@@ -0,0 +1,27 @@
+// // Question # 03 
+
+// var myValue = "Value after variable declaration is : ??"
+// document.write(myValue +"<br>")
+
+// var num = 9;
+// document.write("Initial value"+num)
+
+// ++num;
+// document.write("Value after increment is"+num)
+
+// num = num+7
+
+// // console.log(myValue);
+
+
+// var num1 =2; 
+// var num2 = 3
+
+
+// console.log("2"-7+"hello");
+
+// var userNum = +prompt("Enter your number")
+// var result = userNum+5;
+// console.log(result);
+
+console.log("900">9);
